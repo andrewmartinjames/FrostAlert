223aaddf11d2dbb05081f9a6046d010b3566bc5f
Andrew James <andrew@amjames.net>
2021-04-11 12:43:36 -0400

    Initial iOS app setup

---
+++ iOS/FrostAlert/FrostAlert/FrostAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrostAlertApp.swift
+//  FrostAlert
+//
+//  Created by Andrew James on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FrostAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
