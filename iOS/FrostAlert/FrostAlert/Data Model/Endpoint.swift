b7cc76ff63ff9a0644456f5c50a94e3a395936f6
Andrew James <andrew@amjames.net>
2021-04-13 16:42:46 -0400

    Struct for firebase data

---
+++ iOS/FrostAlert/FrostAlert/Data Model/Endpoint.swift
@@ -0,0 +1,15 @@
+//
+//  Endpoint.swift
+//  FrostAlert
+//
+//  Created by Andrew James on 4/13/21.
+//
+
+import Foundation
+
+struct Endpoint: Identifiable {
+    var id: String
+    var currentHum: Double
+    var currentTemp: Double
+}
+
