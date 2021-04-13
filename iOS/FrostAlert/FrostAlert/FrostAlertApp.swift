1d848db550d928a847543aad9b4c029a2812fb33
Andrew James <andrew@amjames.net>
2021-04-13 16:13:50 -0400

    Setting up firebase integration

--- iOS/FrostAlert/FrostAlert/FrostAlertApp.swift
+++ iOS/FrostAlert/FrostAlert/FrostAlertApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import UIKit // needed for App Delegate functionality to configure Firebase
+import Firebase
+
 
 @main
 struct FrostAlertApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate // confgure Firebase on startup
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure() // sets up firebase configureation
+        return true
     }
 }
