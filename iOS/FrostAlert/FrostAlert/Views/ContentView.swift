633f17716694371e10d537f47b0b32f8c83a8ba3
Andrew James <andrew@amjames.net>
2021-04-21 11:46:33 -0400

    initial main view buildout

--- iOS/FrostAlert/FrostAlert/Views/ContentView.swift
+++ iOS/FrostAlert/FrostAlert/Views/ContentView.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+let primary: Color = .gray
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("DeviceName")
+                .padding()
+                .font(.largeTitle)
+            Text("TMP ºF")
+                .padding()
+                .font(.custom("Temp", size: 40, relativeTo: .largeTitle))
+            Text("RH%")
+                .font(.largeTitle)
+            Text("Relative Humidity")
+                .padding(.bottom)
+                .font(.headline)
+            Text("Reported Weather:")
+                .font(.headline)
+            Text("Cloudy, RTMP ºF")
+                .font(.headline)
+            Image(systemName: "snow")
+                .font(.custom("Symbol", size: 80, relativeTo: .largeTitle))
+                .padding()
+            Button(action: {print("Test")}) {
+                Text("Settings")
+                    .foregroundColor(.blue)
+            }
+        }
+        .foregroundColor(primary)
+        .scaledToFill()
     }
 }
 
