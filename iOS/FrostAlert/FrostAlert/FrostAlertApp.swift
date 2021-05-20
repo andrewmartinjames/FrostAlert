b6322373d20398140c2f160384ce3e9a7df5fc92
Andrew James <andrew@amjames.net>
2021-05-20 15:37:32 -0400

    Added Firebase Authentication with Sign in with Google

--- iOS/FrostAlert/FrostAlert/FrostAlertApp.swift
+++ iOS/FrostAlert/FrostAlert/FrostAlertApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import UIKit // needed for App Delegate functionality to configure Firebase
 import Firebase
+import GoogleSignIn
 
 
 @main
@@ -15,7 +16,8 @@ struct FrostAlertApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate // confgure Firebase on startup
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SignInView()
+            // ContentView()
         }
     }
 }
@@ -23,6 +25,48 @@ struct FrostAlertApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure() // sets up firebase configureation
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        GIDSignIn.sharedInstance().delegate = self
         return true
+    }
+    
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
+      -> Bool {
+      return GIDSignIn.sharedInstance().handle(url)
+    }
+
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
+    }
+}
+
+extension AppDelegate: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+        
+        //handle sign-in errors
+        if let error = error {
+            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+                print("The user has not signed in before or they have since signed out.")
+            } else {
+            print("error signing into Google \(error.localizedDescription)")
+            }
+        return
+        }
+        
+        // Get credential object using Google ID token and Google access token
+        guard let authentication = user.authentication else { return }
+        
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                        accessToken: authentication.accessToken)
+        
+        // Authenticate with Firebase using the credential object
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if let error = error {
+                print("authentication error \(error.localizedDescription)")
+            }
+        }
     }
 }
--- iOS/FrostAlert/FrostAlert/Views/SignInView.swift
+++ iOS/FrostAlert/FrostAlert/Views/SignInView.swift
@@ -6,11 +6,28 @@
 //
 
 import SwiftUI
+import GoogleSignIn
 
 struct SignInView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            SignInButton()
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
+}
+
+struct SignInButton: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> GIDSignInButton {
+        let button = GIDSignInButton()
+        // Customize button here
+        button.colorScheme = .light
+        GIDSignIn.sharedInstance()?.presentingViewController = UIApplication.shared.windows.last?.rootViewController
+        return button
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {}
 }
 
 struct SignInView_Previews: PreviewProvider {
